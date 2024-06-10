9aebf0868e733769a4000f7c59385f18004cbbf5
Bruno Oliveira <brunoserrasilva@gmail.com>
2024-06-10 11:44:04 -0300

    Segundo commit na branch bia-pr

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -4,8 +4,8 @@ import DadosHenrylle from "./DadosHenrylle";
 const About = () => {
   return (
     <div>
-      <h4>Versão 2.1.0</h4>
-      <h5>BIA 18 a 24 de Março/2024</h5>
+      <h4>Versão 2.1.1</h4>
+      <h5>BIA - DI/CD</h5>
       <Link to="/">Voltar</Link>
       <DadosHenrylle />
     </div>
